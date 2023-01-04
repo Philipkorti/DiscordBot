2c89b8d4aba6ac20a839c3cc000c95171e91712a
Philip <philip.korthauer@gmx.at>
2023-01-05 00:35:11 +0100

    first commit

---
+++ Discordbot/Moduls/Commands.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace Discordbot.Moduls
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("ping")]
+        public async Task Ping()
+        {
+            await ReplyAsync("Pong");
+        }
+        [Command("Jannek")]
+        public async Task Jannek()
+        {
+             await ReplyAsync("Jannek du stinkst!!!!!!");
+        }
+        [Command("Philip")]
+        public async Task Philip()
+        {
+            await ReplyAsync("Jannek wollen wir ow spielen");
+        }
+
+    }
+}
